83f5337ea32f395ca4068e983e3f7e416b7bb451
Alexander Osadchy <a.i.osadchy@gmail.com>
2022-12-02 17:02:30 +0300

    implement uniform class and add support for most types

--- src/openway/gl/shader_program.cpp
+++ src/openway/gl/shader_program.cpp
@@ -8,29 +8,73 @@
 #include "openway/gl/call.hpp"
 #include "openway/log.hpp"
 
+namespace {
+
+#define DEFINE_UNIFORM_SETTER(value_type, function_suffix, ...)          \
+    void operator()(const value_type &v) const {                         \
+        OW_GL_CALL(glUniform##function_suffix(m_location, __VA_ARGS__)); \
+    }
+
+class UniformSetter {
+public:
+    explicit UniformSetter(GLint location)
+    : m_location{location} {
+    }
+
+    DEFINE_UNIFORM_SETTER(GLfloat, 1f, v)
+    DEFINE_UNIFORM_SETTER(glm::vec1, 1f, v.x)
+    DEFINE_UNIFORM_SETTER(glm::vec2, 2f, v.x, v.y)
+    DEFINE_UNIFORM_SETTER(glm::vec3, 3f, v.x, v.y, v.z)
+    DEFINE_UNIFORM_SETTER(glm::vec4, 4f, v.x, v.y, v.z, v.w)
+    DEFINE_UNIFORM_SETTER(GLint, 1i, v)
+    DEFINE_UNIFORM_SETTER(glm::ivec1, 1i, v.x)
+    DEFINE_UNIFORM_SETTER(glm::ivec2, 2i, v.x, v.y)
+    DEFINE_UNIFORM_SETTER(glm::ivec3, 3i, v.x, v.y, v.z)
+    DEFINE_UNIFORM_SETTER(glm::ivec4, 4i, v.x, v.y, v.z, v.w)
+    DEFINE_UNIFORM_SETTER(GLuint, 1i, v)
+    DEFINE_UNIFORM_SETTER(glm::uvec1, 1i, v.x)
+    DEFINE_UNIFORM_SETTER(glm::uvec2, 2i, v.x, v.y)
+    DEFINE_UNIFORM_SETTER(glm::uvec3, 3i, v.x, v.y, v.z)
+    DEFINE_UNIFORM_SETTER(glm::uvec4, 4i, v.x, v.y, v.z, v.w)
+    DEFINE_UNIFORM_SETTER(glm::mat2, Matrix2fv, 1, 0, glm::value_ptr(v))
+    DEFINE_UNIFORM_SETTER(glm::mat3, Matrix3fv, 1, 0, glm::value_ptr(v))
+    DEFINE_UNIFORM_SETTER(glm::mat4, Matrix4fv, 1, 0, glm::value_ptr(v))
+    DEFINE_UNIFORM_SETTER(glm::mat2x3, Matrix2x3fv, 1, 0, glm::value_ptr(v))
+    DEFINE_UNIFORM_SETTER(glm::mat3x2, Matrix3x2fv, 1, 0, glm::value_ptr(v))
+    DEFINE_UNIFORM_SETTER(glm::mat2x4, Matrix2x4fv, 1, 0, glm::value_ptr(v))
+    DEFINE_UNIFORM_SETTER(glm::mat4x2, Matrix4x2fv, 1, 0, glm::value_ptr(v))
+    DEFINE_UNIFORM_SETTER(glm::mat3x4, Matrix3x4fv, 1, 0, glm::value_ptr(v))
+    DEFINE_UNIFORM_SETTER(glm::mat4x3, Matrix4x3fv, 1, 0, glm::value_ptr(v))
+
+private:
+    GLint m_location;
+};
+
+#undef DEFINE_UNIFORM_SETTER
+
+} // namespace
 
 ShaderProgram::LinkingError::LinkingError(GLuint descriptor)
-    : std::runtime_error("shader program linking error")
-    , m_descriptor(descriptor) {
+: std::runtime_error("shader program linking error")
+, m_descriptor(descriptor) {
 }
 
 GLuint ShaderProgram::LinkingError::get_descriptor() const {
     return m_descriptor;
 }
 
-
 ShaderProgram::ShaderProgram(Shader &&vertex, Shader &&fragment)
-    : Descriptor(OW_GL_CALL(glCreateProgram()))
-    , m_vertex_shader(std::move(vertex))
-    , m_fragment_shader(std::move(fragment))
-    , m_uniform_locations() {
+: Descriptor(OW_GL_CALL(glCreateProgram()))
+, m_vertex_shader(std::move(vertex))
+, m_fragment_shader(std::move(fragment))
+, m_uniform_locations() {
     OW_GL_CALL(glAttachShader(*this, m_vertex_shader));
     OW_GL_CALL(glAttachShader(*this, m_fragment_shader));
     OW_GL_CALL(glLinkProgram(*this));
 
     GLint success;
     OW_GL_CALL(glGetProgramiv(*this, GL_LINK_STATUS, &success));
-    if(!success) {
+    if (!success) {
         OW_LOG_THROW LinkingError{*this};
     }
 }
@@ -45,44 +89,29 @@ void ShaderProgram::use() {
     OW_GL_CALL(glUseProgram(*this));
 }
 
-void ShaderProgram::set_uniform(const std::string &name, const glm::mat4 &matrix) {
+void ShaderProgram::set_uniform(const std::string &name, const Uniform &uniform) {
     GLint location = get_uniform_location(name);
     // TODO: use glProgramUniform or something else to preserve uniform values
-    OW_GL_CALL(glUniformMatrix4fv(location, 1, GL_FALSE, glm::value_ptr(matrix)));
-}
-
-void ShaderProgram::set_uniform(const std::string &name, const glm::vec3 &vector) {
-    GLint location = get_uniform_location(name);
-    OW_GL_CALL(glUniform3fv(location, 1, glm::value_ptr(vector)));
-}
-
-void ShaderProgram::set_uniform(const std::string &name, float scalar) {
-    GLint location = get_uniform_location(name);
-    OW_GL_CALL(glUniform1f(location, scalar));
-}
-
-void ShaderProgram::set_uniform(const std::string &name, int scalar) {
-    GLint location = get_uniform_location(name);
-    OW_GL_CALL(glUniform1i(location, scalar));
+    std::visit(UniformSetter{location}, uniform);
 }
 
 GLint ShaderProgram::get_uniform_location(const std::string &name) {
     auto it = m_uniform_locations.find(name);
     if (it == m_uniform_locations.end()) {
-        GLint location = OW_GL_CALL(glGetUniformLocation(*this, name.c_str()));
-        it = m_uniform_locations.insert({name, location}).first;
+        GLint loc = OW_GL_CALL(glGetUniformLocation(*this, name.c_str()));
+        it        = m_uniform_locations.insert({name, loc}).first;
     }
     return it->second;
 }
 
 ShaderProgram ShaderProgram::load_from_files(
-        const std::string &vertex,
-        const std::string &fragment
+    const std::string &vertex,
+    const std::string &fragment
 ) {
     try {
         return ShaderProgram{
             Shader::load_from_file(GL_VERTEX_SHADER, vertex),
-            Shader::load_from_file(GL_FRAGMENT_SHADER, fragment)
+            Shader::load_from_file(GL_FRAGMENT_SHADER, fragment),
         };
     } catch (const LinkingError &error) {
         // TODO: remove this size gap, maybe move log to LinkingError
